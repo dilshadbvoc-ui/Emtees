ce4930f87a62c0bfedcdcde09cfd4463f705b0f2
Prohostix <your-prohostix-email@example.com>
2026-08-10 02:47:29 +0530

    fix: increase json and urlencoded payload limits to 50mb

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,8 +30,8 @@ process.stdout.write = function (chunk: any, encoding?: any, callback?: any): bo
 const app = express();
 
 app.use(cors({ origin: "*", credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // tRPC express middleware
 app.use(
